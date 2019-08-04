4b2f59d8ab168a15558b5442baf4a382a052941f
Cydney <johnson_cydney@yahoo.com>
2019-08-04 15:24:15 -0500

    cyd

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session   
 
 from flask_sqlalchemy import SQLAlchemy
-
 
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:basketball@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:HoopDreams1!@localhost:8889/blogz'
 
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -23,23 +22,61 @@ class Blog(db.Model):
 
 
     id = db.Column(db.Integer, primary_key=True)
-
     title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))   
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
 
-    body = db.Column(db.String(1000))
 
 
 
-    def __init__(self,title,body):
+
+    def __init__(self,title,body,owner):
 
         self.title = title
 
         self.body = body
 
+        self.owner = owner
+class User(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(120), unique=True)
+   password = db.Column(db.String(120))
+   blogs = db.relationship('Blog', backref='owner')
+   def __init__(self, username, password):
+       self.username = username
+       self.password = password
+
+@app.before_request
+def require_login():
+   allowed_routes = ['login', 'register']
+   if request.endpoint not in allowed_routes and 'email' not in session:
+       return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged In")
+            return redirect('/newpost')
+        if user and user.password != password:
+            flash("User password Incorrect", "error")
+            return redirect('/login')
+        if not user:
+            flash("User Does Not Exisit", "error")
+            return redirect('/login')    
+        
+
+    return render_template('login.html')
 
 
-@app.route("/blog", methods=['POST','GET'])
 
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def index():
 
     blogid = request.args.get('id')
@@ -49,7 +86,6 @@ def index():
     entries = Blog.query.all()
     
     return render_template('todos.html',entries =entries) 
-
 
 
 
@@ -81,8 +117,6 @@ def newpost():
 
 
     return render_template('new_post.html')
-
-    
 
 
 
